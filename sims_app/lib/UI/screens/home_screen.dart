9aeab402e1e39f9cb39d5241d262313d063b10fe
Maheash <maheash.mmk17@gmail.com>
2023-02-16 00:18:06 +0530

    final for the day

--- sims_app/lib/UI/screens/home_screen.dart
+++ sims_app/lib/UI/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:SIMS/UI/auth/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/utils.dart';
@@ -12,7 +13,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool loading = false;
   final auth = FirebaseAuth.instance;
+  final databaseRef = FirebaseDatabase.instance.ref('realtimeSoilData/Land1');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +35,16 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: Icon(Icons.logout_outlined)),
           const SizedBox(width: 10),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
       ),
     );
   }
