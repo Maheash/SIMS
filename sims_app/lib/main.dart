a0aaf34194048b3eaf63d0874ebbf5f328290ddb
Maheash <maheash.mmk17@gmail.com>
2023-03-21 15:22:59 +0530

    cleaning unused pages

--- sims_app/lib/main.dart
+++ sims_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:SIMS/UI/splash_screen.dart';
-import 'package:SIMS/value.dart';
-// import 'package:SIMS/value.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -25,15 +23,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SIMS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       // home: SplashScreen(),
@@ -54,66 +43,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   var value;
-//   final DatabaseReference _valRef =
-//       FirebaseDatabase.instance.ref('realtimeSoilData/Land1');
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _activateListeners();
-//   }
-
-//   void _activateListeners() {
-//     _valRef.child("realtimeSoilData/test").onValue.listen((event) {
-//       final description = event.snapshot.value;
-//       setState(() {
-//         value='Today\'s Special: $description.toString()';
-//       });
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'Water level is at $value',
-//             ),
-//           ],
-//         ),
-//       ),
-//       // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
